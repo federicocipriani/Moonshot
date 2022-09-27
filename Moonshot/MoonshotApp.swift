f19bbdd41e5deed1e76668b1182c9f02b253f261
federicocipriani <federicocipriani.fc@gmail.com>
2022-09-27 15:07:56 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Federico Cipriani on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
